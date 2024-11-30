85e142d8ff82836a90212e0714f7a73af58aaa41
Takahito Urai <uraitakahito@gmail.com>
2024-12-01 06:03:45 +0900

    ESLint support

--- test-jest/index.test.js
+++ test-jest/index.test.js
@@ -1,33 +1,37 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
-const EXTENSION_PATH = "./src";
+const EXTENSION_PATH = './src';
 // How is the Chrome Extension ID of an unpacked extension generated?
 // https://stackoverflow.com/questions/26053434/how-is-the-chrome-extension-id-of-an-unpacked-extension-generated
-const EXTENSION_ID = "jkomgjfbbjocikdmilgaehbfpllalmia";
+const EXTENSION_ID = 'jkomgjfbbjocikdmilgaehbfpllalmia';
 
 let browser;
 
-beforeEach(async () => {
-  browser = await puppeteer.launch({
-    headless: false,
-    args: [
-      `--disable-extensions-except=${EXTENSION_PATH}`,
-      `--load-extension=${EXTENSION_PATH}`,
-    ],
+describe('popup', () => {
+  beforeEach(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      args: [
+        `--disable-extensions-except=${EXTENSION_PATH}`,
+        `--load-extension=${EXTENSION_PATH}`,
+      ],
+    });
   });
-});
 
-afterEach(async () => {
-  await browser.close();
-  browser = undefined;
-});
+  afterEach(async () => {
+    await browser.close();
+    browser = undefined;
+  });
 
-test("popup renders correctly", async () => {
-  const page = await browser.newPage();
-  await page.goto(`chrome-extension://${EXTENSION_ID}/popup.html`);
+  it('popup renders correctly', async () => {
+    expect.assertions(1);
 
-  const list = await page.$("ul");
-  const children = await list.$$("li");
+    const page = await browser.newPage();
+    await page.goto(`chrome-extension://${EXTENSION_ID}/popup.html`);
 
-  expect(children.length).toBe(1);
+    const list = await page.$('ul');
+    const children = await list.$$('li');
+
+    expect(children).toHaveLength(1);
+  });
 });
