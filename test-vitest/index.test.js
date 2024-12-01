36b80cf0a3fbdbdc396be3b571ea6dc2de3edb87
Takahito Urai <uraitakahito@gmail.com>
2024-12-02 05:08:20 +0900

    Refactor test-vitest to use single quotes and improve assertion in Puppeteer tests

--- test-vitest/index.test.js
+++ test-vitest/index.test.js
@@ -1,13 +1,13 @@
-import puppeteer from "puppeteer";
+import puppeteer from 'puppeteer';
 
-const EXTENSION_PATH = "./src";
+const EXTENSION_PATH = './src';
 // How is the Chrome Extension ID of an unpacked extension generated?
 // https://stackoverflow.com/questions/26053434/how-is-the-chrome-extension-id-of-an-unpacked-extension-generated
-const EXTENSION_ID = "jkomgjfbbjocikdmilgaehbfpllalmia";
+const EXTENSION_ID = 'jkomgjfbbjocikdmilgaehbfpllalmia';
 
 let browser;
 
-describe("popup.html", () => {
+describe('popup.html', () => {
   beforeEach(async () => {
     browser = await puppeteer.launch({
       headless: false,
@@ -23,13 +23,15 @@ describe("popup.html", () => {
     browser = undefined;
   });
 
-  it("renders correctly", async () => {
+  it('renders correctly', async () => {
+    expect.assertions(1);
+
     const page = await browser.newPage();
     await page.goto(`chrome-extension://${EXTENSION_ID}/popup.html`);
 
-    const list = await page.$("ul");
-    const children = await list.$$("li");
+    const list = await page.$('ul');
+    const children = await list.$$('li');
 
-    expect(children.length).toBe(1);
+    expect(children).toHaveLength(1);
   });
 });
